b5f48771ed5cfeb31af4afc02feab6f9ae32a19d
Bernardo Laurindo <bernardo.gonzaga@sou.inteli.edu.br>
2025-05-25 23:30:24 -0300

    .

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 
-router.get('/', taskController.getAllTasks);
-router.get('/:id', taskController.getTaskById);
+router.get('/', taskController.getAll);
+router.get('/:id', taskController.getById);
 router.post('/', taskController.createTask);
 router.put('/:id', taskController.updateTask);
 router.delete('/:id', taskController.deleteTask);
