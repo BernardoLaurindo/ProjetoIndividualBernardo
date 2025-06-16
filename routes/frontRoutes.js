2f68787442f46713d9a77db426ee4aa9ec7b685d
Bernardo Laurindo <bernardo.gonzaga@sou.inteli.edu.br>
2025-06-16 11:25:35 -0300

    localhost teste

--- routes/frontRoutes.js
+++ routes/frontRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Página de login
-router.get('/login', (req, res) => {
+router.get('', (req, res) => {
   res.render('layout/main', {
     pageTitle: 'TaskFlow - Login',
     content: 'pages/login',
